d686284b57200babcf2ff7817802c23dc514a26d
tonezone6 <tonezone6@gmail.com>
2023-11-01 12:12:35 +0100

    Add font resources to the package

--- SonoDesign/Package.swift
+++ SonoDesign/Package.swift
@@ -4,20 +4,23 @@
 import PackageDescription
 
 let package = Package(
-    name: "SonoDesign",
-    products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "SonoDesign",
-            targets: ["SonoDesign"]),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "SonoDesign"),
-        .testTarget(
-            name: "SonoDesignTests",
-            dependencies: ["SonoDesign"]),
-    ]
+  name: "SonoDesign",
+  platforms: [.iOS(.v16)],
+  products: [
+    .library(
+      name: "SonoDesign",
+      targets: ["SonoDesign"]
+    )
+  ],
+  targets: [
+    .target(
+      name: "SonoDesign",
+      dependencies: [],
+      resources: [.process("Resources")]
+    ),
+    .testTarget(
+      name: "SonoDesignTests",
+      dependencies: ["SonoDesign"]
+    )
+  ]
 )
