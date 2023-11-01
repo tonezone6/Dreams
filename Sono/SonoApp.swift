9ae0b745d5d61f52b541107a8a75c6f142c6967e
tonezone6 <tonezone6@gmail.com>
2023-11-01 11:54:47 +0100

    Initial Commit

---
+++ Sono/SonoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SonoApp.swift
+//  Sono
+//
+//  Created by Alex S. on 01/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct SonoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
